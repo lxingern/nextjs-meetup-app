db46b11afd5bc560188641db4103db6de55d952d
Xing Ern <lxingern@gmail.com>
2023-02-21 16:41:28 +0800

    fetch meetups data from database for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from 'mongodb'
 import MeetupList from '../components/meetups/MeetupList'
-
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!'
-    }
-]
 
 function HomePage(props) {
     return <MeetupList meetups={props.meetups} />
@@ -34,9 +18,23 @@ function HomePage(props) {
 
 export async function getStaticProps() {
     // fetch data
+    const client = await MongoClient.connect(`mongodb+srv://xingern:${process.env.mongoDBPassword}@cluster0.p16tjw1.mongodb.net/meetups?retryWrites=true&w=majority`)
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups')
+
+    const meetups = await meetupsCollection.find().toArray()
+
+    client.close()
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString()
+            }))
         },
         revalidate: 10
     }
